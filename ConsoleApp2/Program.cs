ef0d97bf4ae1225f065af616e991043b36d926ba
Deivid <deividhgcamargos@gmail.com>
2021-07-07 17:50:06 -0300

    novo ola mundo

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola mundo");
+
         }
     }
 }
