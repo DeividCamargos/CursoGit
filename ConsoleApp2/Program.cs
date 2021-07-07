5d6676af12bcf8ad3909eb286ca29d6631e5d8c0
Deivid <deividhgcamargos@gmail.com>
2021-07-07 17:50:30 -0300

    Cu

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Ola mundo");
+            Console.WriteLine("Cu");
 
         }
     }
